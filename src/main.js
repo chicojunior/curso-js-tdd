bb298decc8a99963110bb438d51ab7bfe7512c13
Francisco do vale <francisco.vale@iviatecnologia.com.br>
2018-12-14 14:30:33 -0300

    Retorno ao curso

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const a = 3;
+const b = 2;
+
+const c = 'fdhsfjkdsahfjkdaslhfjkdaslhfjkdsalhfjkdslhfjsdklfhjsdlkahfjklsdahjfklhsdajfhjsdfhjskdalfhjksdafhjksdflhjsdkahlfjksdalhfjdskalfhjk';
+
+function sum() {
+  return a + b + c;
+}
+
+sum();
