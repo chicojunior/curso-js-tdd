9ad8baf608ba5e60656af784d2a95af850d65f47
Francisco do vale <francisco.vale@iviatecnologia.com.br>
2019-04-04 17:15:24 -0300

    Assitido até template literals

--- src/main.js
+++ src/main.js
@@ -1,10 +1,39 @@
-const a = 3;
-const b = 2;
+/* eslint-disable arrow-parens */
+/* eslint-disable no-unused-vars */
 
-const c = 'fdhsfjkdsahfjkdaslhfjkdaslhfjkdsalhfjkdslhfjsdklfhjsdlkahfjklsdahjfklhsdajfhjsdfhjskdalfhjksdafhjksdflhjsdkahlfjksdalhfjdskalfhjk';
+const a = 3
+const b = 2
+
+const c = 'fdhsfjkdsahfjkdaslhfjkdaslhfjkdsalhfjkdslhfjsdkl'
 
 function sum() {
-  return a + b + c;
+  return a + b + c
 }
 
-sum();
+const test = sum()
+
+console.log(test)
+
+const australia = ['Adelaide', 'Brisbane', 'Sydney']
+
+// ES5 function
+const love = australia.map(function(city) {
+  return `I love ${city}`
+})
+
+// ES6 arrow function
+const loveArrow = australia.map((city) => {
+  return `I love ${city}`
+})
+
+// ES6 arrow function single parameter
+const loveArrowSingleArgument = australia.map(city => {
+  return `I love ${city}`
+})
+
+// ES6 arrow function one line
+const loveArrowOneLine = australia.map(city => `I love ${city}`)
+
+const loveChain = australia.filter(city => city === 'Adelaide').map(city => `I love ${city}`)
+
+console.log(loveChain)
